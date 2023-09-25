5a5cc618a37631feb5e1972a42635a68bf1f6287
Jun Bo Lau <laujunbo@gmail.com>
2023-09-25 15:30:41 -0400

    Added potential curves and split into Magma batches

---
+++ plane_quintic/curve_check.m
@@ -0,0 +1,52 @@
+/*curve_check.m
+
+  Use this script to generate torsion prime data:
+
+    ls ./data/ | parallel  "magma -b InputFileName:={} curve_check.m"
+
+  This will take the data files in `./data/`, and for each curve in each file,
+  will generate a new file (appended with `with_genus`) in the same directory.
+
+*/
+
+
+RealInputFileName := "./data/" cat InputFileName;
+OutputFileName := "./data/" cat "with_genus_" cat InputFileName;
+
+LinesOfInputFile := Split(Read(RealInputFileName), "\n");
+
+R<x0,x1,x2> := PolynomialRing(GF(2),3);
+X := ProjectiveSpace(R);
+monos5 := [x0^5, x0^4*x1, x0^4*x2, x0^3*x1^2, x0^3*x1*x2, x0^3*x2^2, x0^2*x1^3, x0^2*x1^2*x2, x0^2*x1*x2^2, x0^2*x2^3, x0*x1^4, x0*x1^3*x2, x0*x1^2*x2^2, x0*x1*x2^3, x0*x2^4, x1^5, x1^4*x2, x1^3*x2^2,x1^2*x2^3, x1*x2^4, x2^5];
+
+GenusCheck := function(_fsupp)
+    fsupp := eval(_fsupp);
+    pol := R!0;
+    for i in fsupp do
+        pol +:= monos5[i+1];
+    end for;
+    Y := Scheme(X,pol);
+    if Dimension(Y) eq 1 and IsIrreducible(Y) eq true or IsReduced(Y) eq true then
+        C := Curve(Y);
+        F0 := FunctionField(C);
+        F := AlgorithmicFunctionField(F0);
+        if Genus(F) eq 6 then
+            return true;
+        else
+            return false;
+        end if; 
+    else
+        return false;
+    end if;
+end function;
+    
+
+for MyLine in LinesOfInputFile do
+    if GenusCheck(MyLine) eq true then
+        to_print := MyLine cat "\n";
+        fprintf OutputFileName, to_print;
+    end if;
+end for;
+
+quit;
+    
