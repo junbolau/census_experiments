0b59babfaa3b0a9865baf1eecb6478bea470ef33
Jun Bo Lau <laujunbo@gmail.com>
2023-09-27 11:06:33 -0400

    Tested with new isom_check and generated new data

--- helper_ref/depracated_isom_class_check.m
+++ helper_ref/depracated_isom_class_check.m
@@ -31,10 +31,10 @@
 
 */
 
+
 OutputFileName := "isomclass_" cat InputFileName;
 LinesOfInputFile := Split(Read(InputFileName), "\n");
 
-// Count number of lines in text file
 function LineCount(F)
     FP := Open(F, "r");
     count := 0;
@@ -54,7 +54,7 @@ R<x0,x1,x2>:= PolynomialRing(GF(2),3);
 X := ProjectiveSpace(R);
 monos5 := [x0^5, x0^4*x1, x0^4*x2, x0^3*x1^2, x0^3*x1*x2, x0^3*x2^2, x0^2*x1^3, x0^2*x1^2*x2, x0^2*x1*x2^2, x0^2*x2^3, x0*x1^4, x0*x1^3*x2, x0*x1^2*x2^2, x0*x1*x2^3, x0*x2^4, x1^5, x1^4*x2, x1^3*x2^2,x1^2*x2^3, x1*x2^4, x2^5];
 
-// Quick construction of function field with IsIsomorphic functionality
+
 function FFConstruction(fsupp)
     pol := R!0;
     for k in fsupp do
@@ -66,46 +66,51 @@ function FFConstruction(fsupp)
     return AlgorithmicFunctionField(F_);
 end function;
 
-// Each line is a support ordered by point count, so we need to get starting and ending indices
-function CountIndices(TxtFile, InitialPointCounts,StartingIndex)
-    L := #TxtFile;
-    for k in [StartingIndex..L] do
-        tmp := eval(TxtFile[k]);
-        if tmp[1] eq InitialPointCounts then
-            continue;
-        else
-            return k-1;
-        end if;
-    end for;
-    return L;
-end function;
 
-// Main loop: check for pairwise isomorphism by varying over elements of the same point counts
 lst := eval(LinesOfInputFile[1]);
-final := [lst[2]];
-i := 1;
-while i le L do
-    lst := eval(LinesOfInputFile[i]);
-    ct := lst[1];
-    supp := lst[2];
-    F0 := FFConstruction(supp);
+pol := R!0;
+for k in lst[2] do
+    pol +:= monos5[k+1];
+end for;
 
-    tmp := [F0];
-    supptmp := [supp];
-    j := CountIndices(LinesOfInputFile,ct,i);
-    for ind in [i..j] do
-        lst2 := eval(LinesOfInputFile[ind]);
-        supp2 := lst2[2];
-        F02 := FFConstruction(supp2);
-        if forall(u){m : m in tmp | IsIsomorphic(F02,m) eq false } eq true then
-            Append(~tmp,F02);
-            Append(~supptmp,supp2);
-        end if;
-    end for;
-    for eqn in supptmp do
-        fprintf OutputFileName, "%o" cat "\n", eqn;
-    end for;
-    i := j + 1;
-end while;
+final := [lst[2]];
+ignore_lst := {};
+
+
+for i in [1..L] do
+    if i in ignore_lst eq false then
+    
+        lst := eval(LinesOfInputFile[i]);
+        ct := lst[1];
+        supp := lst[2];
+        F0 := FFConstruction(supp);
+        
+        tmp := [F0];
+        Include(~ignore_lst,i);
+        
+        for j in [i+1..L] do
+            lst2 := eval(LinesOfInputFile[j]);
+            ct1 := lst2[1];
+            supp1 := lst2[2];
+            if ct eq ct1 then
+                F0g := FFConstruction(supp1);
+                if forall(u){m : m in tmp | IsIsomorphic(F0g,m) eq false } eq true then
+                    Append(~tmp, F0g);
+                    Append(~final,supp1);
+                    Include(~ignore_lst,j);
+                    end if;
+            else
+                continue;
+            end if;
+        end for;
+    else
+        continue;
+    end if;
+end for;
+
+
+for i in final do
+    fprintf OutputFileName, "%o" cat "\n", i;
+end for;
 
 quit;
