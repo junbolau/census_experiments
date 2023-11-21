25cbaeae33f9f0cef9a6c3bc81df17a64addd478
Jun Bo Lau <laujunbo@gmail.com>
2023-11-21 18:35:26 -0500

    Quick update

---
+++ tmp/sorted/fromssh/add_aut.m
@@ -0,0 +1,44 @@
+OutputFileName := "aut_" cat InputFileName;
+LinesOfInputFile := Split(Read(InputFileName), "\n");
+
+function LineCount(F)
+    FP := Open(F, "r");
+    count := 0;
+    while true do
+        line := Gets(FP);
+        if IsEof(line) then
+            break;
+        end if;
+        count +:= 1;
+    end while;
+    return count;
+end function;
+
+L := LineCount(InputFileName);
+
+R<x0,x1,x2>:= PolynomialRing(GF(2),3);
+X := ProjectiveSpace(R);
+monos5 := [x0^5, x0^4*x1, x0^4*x2, x0^3*x1^2, x0^3*x1*x2, x0^3*x2^2, x0^2*x1^3, x0^2*x1^2*x2, x0^2*x1*x2^2, x0^2*x2^3, x0*x1^4, x0*x1^3*x2, x0*x1^2*x2^2, x0*x1*x2^3, x0*x2^4, x1^5, x1^4*x2, x1^3*x2^2,x1^2*x2^3, x1*x2^4, x2^5];
+
+function FFConstruction(fsupp)
+    pol := R!0;
+    for k in fsupp do
+        pol +:= monos5[k+1];
+        end for;
+    Y_ := Scheme(X,pol);
+    C_ := Curve(Y_);
+    return [pol,#AutomorphismGroup(C_)];
+end function;
+
+lst := eval(LinesOfInputFile[1]);
+
+i := 1;
+
+while i le L do
+    lst := eval(LinesOfInputFile[i]);
+    output := FFConstruction(lst);
+    fprintf OutputFileName, "%o" cat "\n", output;
+    i +:= 1;
+end while;
+
+quit;
