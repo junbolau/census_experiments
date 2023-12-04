6a5233bf78e0e96669ebdfceeebba0e21ff143ae
Jun Bo Lau <laujunbo@gmail.com>
2023-12-04 02:58:11 -0500

    redoing plane quintics

--- plane_quintic/curve_check.m
+++ plane_quintic/curve_check.m
@@ -41,11 +41,7 @@ X := ProjectiveSpace(R);
 monos5 := [x0^5, x0^4*x1, x0^4*x2, x0^3*x1^2, x0^3*x1*x2, x0^3*x2^2, x0^2*x1^3, x0^2*x1^2*x2, x0^2*x1*x2^2, x0^2*x2^3, x0*x1^4, x0*x1^3*x2, x0*x1^2*x2^2, x0*x1*x2^3, x0*x2^4, x1^5, x1^4*x2, x1^3*x2^2,x1^2*x2^3, x1*x2^4, x2^5];
 
 GenusCheck := function(_fsupp)
-    fsupp := eval(_fsupp);
-    pol := R!0;
-    for i in fsupp do
-        pol +:= monos5[i+1];
-    end for;
+    pol := eval(_fsupp);
     Y := Scheme(X,pol);
     if Dimension(Y) eq 1 and IsIrreducible(Y) eq true and IsReduced(Y) eq true then
         C := Curve(Y);
