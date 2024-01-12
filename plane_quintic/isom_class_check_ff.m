a9f0d31894b4d361a6a156843304d50bc9dae6fe
Jun Bo Lau <laujunbo@gmail.com>
2024-01-12 09:51:29 -0500

    flat 9

--- plane_quintic/isom_class_check_ff.m
+++ plane_quintic/isom_class_check_ff.m
@@ -12,7 +12,7 @@
   ./RUN
   
   Comments on the current method:
-  - find ./ -type f | grep txt | grep genus  | perl -ne 'chomp;s/\.\///;print "magma -b InputFileName:=$_ ../isom_class_check.m &\n"' > RUN_isom
+  - find ./ -type f | grep txt | grep genus  | perl -ne 'chomp;s/\.\///;print "magma -b InputFileName:=$_ ../../isom_class_check.m &\n"' > RUN_isom
   - writes a bash script file to run magma file in parallel. need to select files with certain beginnings, #!/bin/sh on top on RUN
   - use several data folders to manage batches (BU server limit)
   - approx 30-35 .txt files in each data folder
